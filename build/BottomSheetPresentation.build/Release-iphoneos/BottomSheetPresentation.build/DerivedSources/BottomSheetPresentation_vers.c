8ee541ac7150a6cfdc6142f18f497e46a2c09604
Jeff Kelley <SlaunchaMan@gmail.com>
2018-07-20 14:00:09 -0400

    Remove build directory.

--- build/BottomSheetPresentation.build/Release-iphoneos/BottomSheetPresentation.build/DerivedSources/BottomSheetPresentation_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char BottomSheetPresentationVersionString[];
- extern const double BottomSheetPresentationVersionNumber;
-
- const unsigned char BottomSheetPresentationVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:BottomSheetPresentation  PROJECT:BottomSheetPresentation-3" "\n";
- const double BottomSheetPresentationVersionNumber __attribute__ ((used)) = (double)3.;
--- build/BottomSheetPresentation.build/Release-iphoneos/BottomSheetPresentation.build/Objects-normal/armv7/BottomSheetPresentation_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jeff/Projects/Open-Source/BottomSheetPresentation/build/BottomSheetPresentation.build/Release-iphoneos/BottomSheetPresentation.build/DerivedSources/BottomSheetPresentation_vers.c
