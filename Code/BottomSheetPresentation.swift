cbc92c4c4cd3fa311c7320d271affc8a124d9c26
Jeff Kelley <SlaunchaMan@gmail.com>
2018-07-20 13:42:17 -0400

    Make BottomSheetPresentationOptions struct initializer public.

--- Code/BottomSheetPresentation.swift
+++ Code/BottomSheetPresentation.swift
@@ -26,13 +26,33 @@ public struct BottomSheetPresentationOptions {
     public let edgeInsets: UIEdgeInsets
 
     /// The default options that are used when calling `init()` on a
-    /// `BottomSheetPresentationManager` with no options. Uses a `cornerRadius`
-    /// of `10`, a `dimmingViewAlpha` of `0.5`, and an `edgeInsets` of `20`
-    /// points on each side.
+    /// `BottomSheetPresentationManager` with no options.
     public static let defaultOptions = BottomSheetPresentationOptions(
         cornerRadius: 10,
         dimmingViewAlpha: 0.5,
         edgeInsets: UIEdgeInsets(constant: 20))
+
+    /// Creates a new `BottomSheetPresentationOptions` struct.
+    ///
+    /// - Parameters:
+    ///   - cornerRadius: The corner radius to use when displaying the presented
+    ///                   view controller. Defaults to `10`.
+    ///   - dimmingViewAlpha: The `alpha` value for the dimming view used behind
+    ///                       the presented view controller. The color is black.
+    ///                       Defaults to `0.5`.
+    ///   - edgeInsets: The amount to inset the presented view controller from
+    ///                 the presenting view controller. This is a minimum; there
+    ///                 may be additional insets depending on the safe area
+    ///                 insets of the presenting view controller’s view (iOS 11
+    ///                 and later). Defaults to edge insets of `20` points on
+    ///                 each side.
+    public init(cornerRadius: CGFloat = 10,
+                dimmingViewAlpha: CGFloat = 0.5,
+                edgeInsets: UIEdgeInsets = UIEdgeInsets(constant: 20)) {
+        self.cornerRadius = cornerRadius
+        self.dimmingViewAlpha = dimmingViewAlpha
+        self.edgeInsets = edgeInsets
+    }
 }
 
 extension BottomSheetPresentationOptions: Equatable {}
