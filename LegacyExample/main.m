f2ee0a55c911e1b6cb66f80c0c3c614205f63c8d
Jeff Kelley <SlaunchaMan@gmail.com>
2019-02-07 16:49:17 -0500

    Update code copyright.

--- LegacyExample/main.m
+++ LegacyExample/main.m
@@ -3,7 +3,7 @@
 //  LegacyExample
 //
 //  Created by Jeff Kelley on 7/19/18.
-//  Copyright © 2018 Detroit Labs. All rights reserved.
+//  Copyright © 2019 Detroit Labs. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
