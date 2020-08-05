fe12478fbf6268ea882ed0808315848292080230
Jeff Kelley <SlaunchaMan@gmail.com>
2020-08-05 15:53:08 -0400

    Remove unused code.

--- Sources/EdgeInsetsHelpers.swift
+++ Sources/EdgeInsetsHelpers.swift
@@ -129,18 +129,3 @@ extension UIEdgeInsets: DirectionalEdgeInsetsConvertible {
     }
 
 }
-
-public extension UIEdgeInsets {
-
-    /// Forms a union with the given edge insets. Each value in the insets will
-    /// be the larger of the two values.
-    ///
-    /// - Parameter otherInsets: The other insets with which to form a union.
-    mutating func formUnion(with otherInsets: UIEdgeInsets) {
-        top = max(top, otherInsets.top)
-        left = max(left, otherInsets.left)
-        bottom = max(bottom, otherInsets.bottom)
-        right = max(right, otherInsets.right)
-    }
-
-}
